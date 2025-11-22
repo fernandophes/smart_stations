3e1403516eb7aca0b0d4fbb0b94ae39bffef0b2d
Fernando Silva <fernandophes@gmail.com>
2025-11-22 16:39:30 -0300

    Feat: Informações do módulo common

---
+++ common/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module common {
+    exports br.edu.ufersa.cc.seg.common.network;
+    exports br.edu.ufersa.cc.seg.common.utils;
+    exports br.edu.ufersa.cc.seg.common.crypto;
+
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires lombok;
+    requires org.slf4j;
+}
