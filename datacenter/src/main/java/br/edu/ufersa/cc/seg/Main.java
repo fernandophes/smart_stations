acc1caad3fbaaec5581b5c1947acd318a42a1216
Fernando Silva <fernandophes@gmail.com>
2025-11-25 01:04:27 -0300

    feat(datacenter): :tada: inicia o módulo datacenter

---
+++ datacenter/src/main/java/br/edu/ufersa/cc/seg/Main.java
@@ -0,0 +1,9 @@
+package br.edu.ufersa.cc.seg;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ datacenter/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module datacenter {
+    exports br.edu.ufersa.cc.seg;
+
+    requires common;
+
+    requires lombok;
+    requires org.slf4j;
+}
