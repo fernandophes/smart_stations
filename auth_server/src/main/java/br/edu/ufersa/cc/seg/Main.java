befd7d8f1b9fa6148003bbfc4bd76d8423b110ac
Fernando Silva <fernandophes@gmail.com>
2025-11-28 04:07:41 -0300

    feat(auth): :sparkles: cria o launcher

--- auth_server/src/main/java/br/edu/ufersa/cc/seg/Main.java
+++ auth_server/src/main/java/br/edu/ufersa/cc/seg/Main.java
@@ -1,7 +1,27 @@
 package br.edu.ufersa.cc.seg;
 
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Scanner;
+
+import br.edu.ufersa.cc.seg.auth.AuthServer;
+import br.edu.ufersa.cc.seg.common.factories.EnvOrInputFactory;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        log.info("Iniciando servidor de autenticação...");
+
+        final var input = Optional.ofNullable(Main.class.getResourceAsStream("/env.txt"))
+                .orElse(System.in);
+
+        final var factory = new EnvOrInputFactory(new Scanner(input));
+        final var server = new AuthServer(factory);
+
+        server.start();
+        factory.close();
     }
+
 }
