e17443cdf6f9e99256a67f1b959f9942453dba3d
Fernando Silva <fernandophes@gmail.com>
2025-11-28 01:14:31 -0300

    feat: :sparkles: implementa ServerMessengers SEM criptografia

---
+++ common/src/main/java/br/edu/ufersa/cc/seg/common/concrete_messengers/TcpServerMessenger.java
@@ -0,0 +1,82 @@
+package br.edu.ufersa.cc.seg.common.concrete_messengers;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+import br.edu.ufersa.cc.seg.common.messengers.Message;
+import br.edu.ufersa.cc.seg.common.messengers.ServerMessenger;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TcpServerMessenger implements ServerMessenger {
+
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public class Subscription implements Closeable {
+        private final Function<Message, Message> callback;
+        private Thread thread;
+
+        private AtomicBoolean isRunning = new AtomicBoolean(false);
+
+        private void start() {
+            isRunning.set(true);
+
+            thread = new Thread(() -> {
+                while (isRunning.get()) {
+                    log.info("Aguardando clientes...");
+
+                    final var client = accept();
+                    client.subscribe(callback);
+                }
+            });
+
+            thread.start();
+        }
+
+        @Override
+        public void close() throws IOException {
+            isRunning.set(false);
+            thread.interrupt();
+        }
+
+        @SneakyThrows
+        private TcpMessenger accept() {
+            return new TcpMessenger(serverSocket);
+        }
+    }
+
+    private final ServerSocket serverSocket;
+
+    public TcpServerMessenger() throws IOException {
+        this(new ServerSocket(0));
+    }
+
+    public TcpServerMessenger(final int port) throws IOException {
+        this(new ServerSocket(port));
+    }
+
+    private TcpServerMessenger(final ServerSocket socket) {
+        this.serverSocket = socket;
+    }
+
+    public int getPort() {
+        return serverSocket.getLocalPort();
+    }
+
+    public Subscription subscribe(final Function<Message, Message> callback) {
+        final var subscription = new Subscription(callback);
+        subscription.start();
+        return subscription;
+    }
+
+    @SneakyThrows
+    public void close() {
+        serverSocket.close();
+    }
+
+}
---
+++ common/src/main/java/br/edu/ufersa/cc/seg/common/concrete_messengers/UdpServerMessenger.java
@@ -0,0 +1,95 @@
+package br.edu.ufersa.cc.seg.common.concrete_messengers;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+import br.edu.ufersa.cc.seg.common.messengers.Message;
+import br.edu.ufersa.cc.seg.common.messengers.ServerMessenger;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class UdpServerMessenger implements ServerMessenger {
+
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public class Subscription implements Closeable {
+        private final Function<Message, Message> callback;
+        private Thread thread;
+
+        private AtomicBoolean isRunning = new AtomicBoolean(false);
+
+        private void start() {
+            isRunning.set(true);
+
+            thread = new Thread(() -> {
+                while (isRunning.get()) {
+                    final var client = accept();
+
+                    final var clientSubscription = client.getMessenger().subscribe(callback);
+                    clientSubscription.handleRequest(client.getFirstMessage());
+                }
+            });
+
+            thread.start();
+        }
+
+        @SneakyThrows
+        private ClientRegistration accept() {
+            final var bytes = new byte[1024];
+            final var packet = new DatagramPacket(bytes, bytes.length);
+            socket.receive(packet);
+
+            final var client = new UdpMessenger(packet.getAddress().getHostName(), packet.getPort());
+
+            return new ClientRegistration(client, client.receive(packet));
+        }
+
+        @Override
+        public void close() throws IOException {
+            isRunning.set(false);
+            thread.interrupt();
+        }
+    }
+
+    @Value
+    public static class ClientRegistration {
+        UdpMessenger messenger;
+        Message firstMessage;
+    }
+
+    private final DatagramSocket socket;
+
+    public UdpServerMessenger() throws IOException {
+        this(new DatagramSocket());
+    }
+
+    public UdpServerMessenger(final int port) throws IOException {
+        this(new DatagramSocket(port));
+    }
+
+    private UdpServerMessenger(final DatagramSocket socket) {
+        this.socket = socket;
+    }
+
+    public int getPort() {
+        return socket.getLocalPort();
+    }
+
+    public Subscription subscribe(final Function<Message, Message> callback) {
+        final var subscription = new Subscription(callback);
+        subscription.start();
+        return subscription;
+    }
+
+    public void close() {
+        socket.close();
+    }
+
+}
